cd4d6f596fadfee1e92acd821e4d84380b951e3b
UNNI <49119124+Krish1959@users.noreply.github.com>
2024-10-26 12:49:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.title("🎈 BCA Project")
-st.write( "LLM Agent for Submissions Compliance Validation" )
+st.write( "Agent-based Analyser for Technical and Regulatory Requirements Checks" )
 st.write( "\n\n\n by: Woon Wei & UNNI" )
