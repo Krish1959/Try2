5d6d58cd1517713c73a7f1f832709471719f71e4
UNNI <49119124+Krish1959@users.noreply.github.com>
2024-10-26 12:47:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title("🎈 BCA Project")
 st.write( "LLM Agent for Submissions Compliance Validation" )
-st.write( "\n\n\n by Woorn Wei & UNNI" )
+st.write( "\n\n\n by: Woon Wei & UNNI" )
